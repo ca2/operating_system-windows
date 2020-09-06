a645eac0ce58a0f07b553c7122974db5687c9c8c
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-09-06 04:45:32 -0300

    <3tbs, Mummi and bilbo!!
    ENDESGA, strager, DapeachTV, tomrandall and AdamCYounis streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    -- hi5 contribution...!!

--- draw2d_gdiplus/graphics.cpp
+++ draw2d_gdiplus/graphics.cpp
@@ -5619,13 +5619,6 @@ gdi_fallback:
 
        }
 
-       if (::is_null(m_pfont))
-       {
-
-          __throw(::exception::exception("invalid state"));
-
-       }
-
       if(pszString == nullptr || *pszString == '\0')
 
          return sized(0, 0);
